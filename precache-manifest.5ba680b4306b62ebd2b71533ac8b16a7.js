10a402f5f29eefb889c1d4b31ee17c1038928eb1
Louise Iyengar <lki@louiseiyengar.com>
2019-07-11 08:45:04 -0400

    Updates

---
+++ precache-manifest.5ba680b4306b62ebd2b71533ac8b16a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f984fdabdd102a95cbdb833670b476c",
+    "url": "/directwines/index.html"
+  },
+  {
+    "revision": "0cd7074f784fcd30c630",
+    "url": "/directwines/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e8e9949fd2eca3102540",
+    "url": "/directwines/static/css/main.4ee9f9da.chunk.css"
+  },
+  {
+    "revision": "0cd7074f784fcd30c630",
+    "url": "/directwines/static/js/2.7a5f0050.chunk.js"
+  },
+  {
+    "revision": "e8e9949fd2eca3102540",
+    "url": "/directwines/static/js/main.adaca24e.chunk.js"
+  },
+  {
+    "revision": "0db7fbf76126921a0f05",
+    "url": "/directwines/static/js/runtime~main.1d3c89fb.js"
+  }
+]);
